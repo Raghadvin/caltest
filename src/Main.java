a82d070ef029d2ee7ef057c46e6f5f3308822a97
raghadvin <rsmd1996@gmail.com>
2022-11-15 14:23:13 +0300

    by Vincent

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int x=1,y=2;
+        sum(x,y);
+
+    }
+    private static void sum (int x ,int y){
+        int sum=0;
+        sum=x+y;
+        System.out.println("the sum=" + sum);
+
+    }
+    private static void mul (int x ,int y){
+        int mul=0;
+        mul=x*y;
+        System.out.println("the result = " + mul);
+        System.out.println("the result modified ");
+
+    }
+}
